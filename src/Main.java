e932b56e99610cb38baba3ba7342532e81fc954c
siemens <sema.perstnev.01@mail.ru>
2023-01-23 15:58:32 +0300

    Домашняя работа №10 (Строки), Перстнев С.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        tasks();
+    }
 
+    public static void tasks() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String secondName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + secondName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println("Задача 2");
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperFullName);
+        System.out.println("Задача 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        String correctFullName = fullName1.replace('ё', 'е');
+        System.out.println(correctFullName);
     }
 }
